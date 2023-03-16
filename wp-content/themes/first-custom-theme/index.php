e2835d4f0394e107d57e4192468b00dc9188d19a
ben blue <benjaminblue@gmail.com>
2023-03-16 12:31:57 -0500

    more first steps

---
+++ wp-content/themes/first-custom-theme/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body <?php body_class(); ?> >
+
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="search-box">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+
+                </nav>
+            </section>
+        </header>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                    services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+
+            </div>
+        </div>
+        <footer class="site-footer">
+
+        </footer>
+    </div>
+    
+</body>
+</html>
